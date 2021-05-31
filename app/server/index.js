952ca5192842f4e76d5f96093f55fc5532cd9bfc
Great Nwamadi <nwamadigreat@gmail.com>
2021-05-31 15:33:16 +0100

    to debug

--- app/server/index.js
+++ app/server/index.js
@@ -7,29 +7,37 @@ const session = require('express-session');
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+const register = require("@react-ssr/express/register");
 
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-app.use(session({
-    secret: 'secret',
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    },
-    resave: true,
-    saveUninitialized: false
-}));
+register(app).then(()=> {
 
-app.use('/api', require('./routes/api'));
-app.use(express.static('public'));
+    // All app code here
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+    
+    app.use(morgan('combined'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    
+    app.use(session({
+        secret: 'secret',
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        },
+        resave: true,
+        saveUninitialized: false
+    }));
+    
+    app.use('/api', require('./routes/api'));
+    app.use(express.static('public'));
+    
+    app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
 
-app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
 
+})
